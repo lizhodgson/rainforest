709abb68270fb323437c15d6a341831d670efdd5
lizhodgson <lizhodgson@gmail.com>
2015-02-08 07:54:32 -0500

    generating the products controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
-Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+Rainforest::Application.routes.draw do
+  resources :products
+end
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -53,4 +50,4 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+
